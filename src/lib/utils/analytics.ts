8e6737c9f3481bc5c3a735989c50f089f709bdb7
RiadYan <182303059+RiadYan@users.noreply.github.com>
2025-09-30 22:20:17 +0200

    feat: analytics

--- src/lib/utils/analytics.ts
+++ src/lib/utils/analytics.ts
@@ -1,5 +1,5 @@
-export function trackEvent(action: string, params: Record<string, any> = {}) {
-	if (typeof window !== 'undefined' && (window as any).gtag) {
-		(window as any).gtag('event', action, params);
+export function trackEvent(eventName: string, data: Record<string, any> = {}) {
+	if (typeof window !== 'undefined' && typeof (window as any).umami !== 'undefined') {
+		(window as any).umami.track(eventName, data);
 	}
 }
